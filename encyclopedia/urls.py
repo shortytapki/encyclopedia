94ec83e04947124053b24374f7085a6f386803fa
ShortyTapki <v.v.vinogradov99@gmail.com>
2022-09-07 15:55:38 +0300

    Added gitignore, new template and adding logic

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:page>", views.entry, name="entry"),
     path("random", views.random, name="random"),
-    # path("search/", views.search, name="search"),
-    path("wiki/search/", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("create", views.create_page, name="create_page"),
+    path("publish", views.publish_new_page, name="publish_new_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -24,23 +24,44 @@ def random(request):
 
 
 def search(request):
-    def to_lower(elem):
-        return elem.lower()
     query = request.GET.get('q', '')
-    if query == '':
-        return HttpResponseRedirect(f'http://localhost:8000')
     if query.lower() in list(map(to_lower, util.list_entries())):
         return HttpResponseRedirect(f'http://localhost:8000/wiki/{query}')
     search_results = []
     for elem in util.list_entries():
-        if query in elem:
+        if query.lower() in elem.lower():
             search_results.append(elem)
     return render(request, "encyclopedia/results.html", {
-        "entry": query,
+        "query": query,
         "results": search_results,
         "matches": len(search_results) > 0
     })
 
 
+def create_page(request):
+    return render(request, "encyclopedia/new.html", {
+        "page_exists": False
+    })
+
+
+def publish_new_page(request):
+    md = request.GET.get('page_text')
+    html = parse_md(md)
+    name = html[html.find('<h1>') + 4: html.find('</h1>')]
+    if name.lower() in list(map(to_lower, util.list_entries())):
+        return render(request, "encyclopedia/new.html", {
+            "page_exists": True,
+            "name": name
+        })
+    else:
+        util.save_entry(name, md)
+        return HttpResponseRedirect(f'wiki/{name}')
+
+
+
 def parse_md(content):
     return markdown(content) if content else content
+
+
+def to_lower(elem):
+    return elem.lower()
